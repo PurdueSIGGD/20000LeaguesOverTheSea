927ad07aa0b3cefa6190560bfa7c906bc93f740e
Rickeon <Rickeon@7d2ba457-d7c3-4905-95e4-8f938edf1c07>
2013-09-04 23:48:43 +0000

    Space Game: Actually added bullets for real.

---
+++ Assets/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision coll)
+    {
+        Collider other = coll.collider;
+        if (other.tag == "Ship" || other.tag == "Player")
+        {
+            //need real death stuff
+            GameObject.DestroyObject(coll.gameObject);
+
+        }
+        GameObject.DestroyObject(this.gameObject);
+    }
+}
