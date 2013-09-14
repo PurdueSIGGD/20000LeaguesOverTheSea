5c9d00aafca9a4ba941ce51346c82b87e0ca7f27
Hayden Thomas <Hayden Thomas@7d2ba457-d7c3-4905-95e4-8f938edf1c07>
2013-09-14 23:19:50 +0000

    Added enemy and made it random spawn around CenterOfGravity

---
+++ Assets/AimAtPlayer.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AimAtPlayer: MonoBehaviour {
+	
+	GameObject parent;
+    GameObject bullet;
+	GameObject playerObject;
+	Orbit parentOrbit;
+	public bool showLine=false;
+	public float shotVelocity=30;
+	int frameCount = 0;
+	
+	// Use this for initialization
+	void Start () {
+		//playerObject = GameObject.Find("Player");
+		parent=this.GetComponent<AttachGun>().parent;
+		parentOrbit=parent.GetComponent<Orbit>();
+        bullet = (GameObject)Resources.Load("SpaceBullet");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerObject == null) {
+			playerObject = GameObject.Find("Player");
+			if (playerObject == null) {
+				return;
+			}
+		}
+			
+		Vector3 direction = playerObject.transform.position - this.transform.position;
+		direction.Normalize();
+		
+		Vector3 initialPosition= this.transform.position + direction * 3;
+		Vector3 initialVelocity = direction * shotVelocity + playerObject.rigidbody.velocity;
+		
+		if (showLine) {
+		
+			Vector3[] first = parentOrbit.getNextPosAndVel(initialPosition,initialVelocity);
+			parentOrbit.drawOrbitLine(this.GetComponent<LineRenderer>(),10000,first);
+		}
+		
+		if (frameCount == 75) {
+			
+			GameObject newBullet = (GameObject)Instantiate(bullet,initialPosition,new Quaternion(0,0,0,0));
+        	newBullet.GetComponent<Orbit>().center = this.parent.GetComponent<Orbit>().center;
+        	newBullet.GetComponent<Orbit>().initialForce = 0;
+        	newBullet.rigidbody.AddForce(initialVelocity, ForceMode.VelocityChange);
+			frameCount = 0;
+		
+		} else {
+			frameCount++;
+			
+		}
+	}
+}
--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -2,26 +2,31 @@
 using System.Collections;
 
 public class Bullet : MonoBehaviour {
-
+	
+	int lifeSpan = 500;
+		
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		lifeSpan--;
+		if (lifeSpan == 0)
+			GameObject.DestroyObject(this.gameObject);
 	}
 
     void OnCollisionEnter(Collision coll)
     {
         Collider other = coll.collider;
-        if (other.tag == "Ship" || other.tag == "Player")
+				
+        if (other.tag == "Ship" || other.tag == "Player" || other.tag == "Enemy")
         {
             //need real death stuff
             GameObject.DestroyObject(coll.gameObject);
-
-        }
+			
+		}
         GameObject.DestroyObject(this.gameObject);
     }
 }
