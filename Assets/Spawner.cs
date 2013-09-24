a740e41ec3085ff8608a309b8a32ee6f7a4d9e3d
jdude0822 <jdude0822@7d2ba457-d7c3-4905-95e4-8f938edf1c07>
2013-09-24 01:01:07 +0000

    Space Game: Changed spawner to handle more than one enemy,  and their spawn chance.

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -1,14 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Spawner : MonoBehaviour {
 	
-	
-	public GameObject spawn;
 	public int spawnTimer=100;
 	public float spawnRadius=100;
 	public int minSpawnRadius=50;
 	int timer;
+	GameObject pickedEnemy;
+	public GameObject[] spawnEnemies;
+	public int[] percentChance; //Chance for enemy to spawn
+	
 	// Use this for initialization
 	void Start () {
 	timer=spawnTimer;
@@ -19,24 +22,42 @@ public class Spawner : MonoBehaviour {
 
 	}
 	
+	// Spawns enemy after specified time (spawnTimer)
 	void FixedUpdate()
 	{
-		
-	
-	timer--;
-	if (timer<=0)
+		timer--;
+		if (timer<=0)
 		{
-	Vector2 point = Random.insideUnitCircle* spawnRadius;
-		
-	while (point.magnitude<minSpawnRadius)
-		{
-			point = Random.insideUnitCircle* spawnRadius;
-		}
-	GameObject newSpawn = (GameObject)Instantiate(spawn,point,new Quaternion(0,0,0,0));
-	newSpawn.GetComponent<Orbit>().center=this.gameObject;
+			pickedEnemy = pickSpawn();
+			if(pickedEnemy != null){
+				Vector2 point = Random.insideUnitCircle* spawnRadius;
 
-	timer=spawnTimer;
+				while (point.magnitude<minSpawnRadius)
+				{
+					point = Random.insideUnitCircle* spawnRadius;
+				}
+				
+				GameObject newSpawn = (GameObject)Instantiate(pickedEnemy,point,new Quaternion(0,0,0,0));
+				newSpawn.GetComponent<Orbit>().center=this.gameObject;
+			}
+			
+			timer = spawnTimer;
 		}
 		
 	}
+	
+	// Picks which enemy to spawn based on % chance and random number
+	public GameObject pickSpawn(){
+		var randomNumber = Random.Range(1,101);
+		int i, k;
+		k = 1;
+		for(i = 0; i < spawnEnemies.Length; i++){
+			if(randomNumber > k && randomNumber < percentChance[i] + k){
+				return spawnEnemies[i];
+			}
+			k += percentChance[i];
+		}
+		return null;
+	}
+	
 }
