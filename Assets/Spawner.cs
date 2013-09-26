aeccfe388e030c0b07a962b62b41a1bdc6740509
jdude0822 <jdude0822@7d2ba457-d7c3-4905-95e4-8f938edf1c07>
2013-09-26 00:26:18 +0000

    Space Game: Added delay functionality to spawner.  More efficient point choosing, changed spawn percentage to a weight.

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -5,15 +5,26 @@ using System.Collections.Generic;
 public class Spawner : MonoBehaviour {
 	
 	public int spawnTimer=100;
-	public float spawnRadius=100;
+	public float maxSpawnRadius=100;
 	public int minSpawnRadius=50;
-	int timer;
+	int timer, totalWeight;
+	float x, y;
+	int sign;
+	int lenAvail;
+	bool complete;
 	GameObject pickedEnemy;
 	public GameObject[] spawnEnemies;
-	public int[] percentChance; //Chance for enemy to spawn
+	protected Vector2 point;
+	public int[] weight; //weight: Chance for enemy to spawn
+	public int[] delay;  //delay: delay in seconds before inserting enemy in spawn sequence.
+	private int[] available;
+	int i;
 	
 	// Use this for initialization
 	void Start () {
+	complete = false;
+	available = new int[spawnEnemies.Length];
+		
 	timer=spawnTimer;
 	}
 	
@@ -25,18 +36,53 @@ public class Spawner : MonoBehaviour {
 	// Spawns enemy after specified time (spawnTimer)
 	void FixedUpdate()
 	{
+		
+		if(!complete)
+		{
+			lenAvail = 0;
+			totalWeight=0;
+			for(i = 0; i < spawnEnemies.Length; i++)
+			{
+				if(delay[i] <= Time.time)
+				{
+					available[lenAvail] = i;
+					lenAvail++;
+					totalWeight += weight[i];
+				}
+			}
+			
+			if(lenAvail == spawnEnemies.Length)
+			{
+				complete = true;
+			}
+		}
+		
 		timer--;
-		if (timer<=0)
+		if (timer <= 0 && lenAvail > 0)
 		{
 			pickedEnemy = pickSpawn();
-			if(pickedEnemy != null){
-				Vector2 point = Random.insideUnitCircle* spawnRadius;
-
-				while (point.magnitude<minSpawnRadius)
+			if(pickedEnemy != null)
+			{
+				sign = Random.Range(0,2);
+				x = Random.Range(minSpawnRadius,maxSpawnRadius);
+				
+				if(sign == 0)
 				{
-					point = Random.insideUnitCircle* spawnRadius;
+					x *= -1;
 				}
 				
+				sign = Random.Range(0,2);
+				
+				y = Random.Range(minSpawnRadius,maxSpawnRadius);
+				
+				if(sign == 0)
+				{
+					y *= -1;
+				}
+				
+				
+
+				point.Set (x,y);
 				GameObject newSpawn = (GameObject)Instantiate(pickedEnemy,point,new Quaternion(0,0,0,0));
 				newSpawn.GetComponent<Orbit>().center=this.gameObject;
 			}
@@ -46,18 +92,24 @@ public class Spawner : MonoBehaviour {
 		
 	}
 	
-	// Picks which enemy to spawn based on % chance and random number
-	public GameObject pickSpawn(){
-		var randomNumber = Random.Range(1,101);
-		int i, k;
+	// Picks which enemy to spawn based on their weight against a random number
+	public GameObject pickSpawn()
+	{
+		var randomNumber = Random.Range(1,totalWeight+1);
+		int k;
 		k = 1;
-		for(i = 0; i < spawnEnemies.Length; i++){
-			if(randomNumber > k && randomNumber < percentChance[i] + k){
-				return spawnEnemies[i];
+		for(i = 0; i < lenAvail; i++)
+		{
+			if(randomNumber >= k && randomNumber < weight[available[i]] + k)
+			{
+				return spawnEnemies[available[i]];
 			}
-			k += percentChance[i];
+			
+			k += weight[available[i]];
 		}
-		return null;
+		
+		return null; //If it gets here then there is an issue
 	}
 	
 }
+
