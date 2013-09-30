f11a3865e8c1904110b425e14a84478667a23e6f
lazopard <lazopard@7d2ba457-d7c3-4905-95e4-8f938edf1c07>
2013-09-30 23:53:59 +0000

    Now the planet has health, it is lowered whenever an object(except for the SpaceBullet) collides with it.

--- Assets/colliders/PlantColllision.cs
+++ Assets/colliders/PlantColllision.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 
 public class PlantColllision : BasicCollision {
+	
+	int health = 100;
 
 	// Use this for initialization
 	void Start () {
@@ -10,11 +12,17 @@ public class PlantColllision : BasicCollision {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (health <= 0) {
+				GameObject.DestroyObject(this.gameObject);
+		}
 	}
 	
 	public override void hit(GameObject collider)
 	{
+		Collider coll = collider.collider;
+		if (coll.tag != "Ship") {
+				health--;
+		}
 		//lower health of planet if hit by planet lowering health sort of thing (not a bullet)
 	}
 }
