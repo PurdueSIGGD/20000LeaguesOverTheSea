03b71c97b46af4b8e0e79303876981ba93faffec
smithsp <smithsp@7d2ba457-d7c3-4905-95e4-8f938edf1c07>
2013-10-02 23:30:48 +0000

    Main Menu and background fix for level.

---
+++ Assets/Menu/Menu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+	/**
+	 * Class where we handle all Menu GUI.
+	 * 
+	 * is Part of the object 'Gui Constructor' on the Menu Scene
+	 */
+	
+	GUIText Title; 
+	GUIText Play;
+	
+	void Start() {
+    	Title = GameObject.Find ("menuTitle").guiText;
+		Play = GameObject.Find ("menuPlay").guiText;
+    }
+	
+	void OnGUI () {
+		//Best way to detect clicks on GuiText
+		if(Input.GetMouseButtonDown(0) && Play.GetScreenRect().Contains(Input.mousePosition))
+		{
+			Application.LoadLevel("first");
+		}
+		
+		if(Input.GetMouseButtonDown(0) && Play.GetScreenRect().Contains(Input.mousePosition))
+		{
+			Application.Quit();	
+		}
+		
+	}
+	
+}
