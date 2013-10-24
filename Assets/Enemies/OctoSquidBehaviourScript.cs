f07cdab8aaf6166208a721c5dfec396572ee8faa
TylerWolverton <TylerWolverton@7d2ba457-d7c3-4905-95e4-8f938edf1c07>
2013-10-24 02:13:38 +0000

    Space Game: Ignore most things to avoid subversion issues, added OctoSquid

---
+++ Assets/Enemies/OctoSquidBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OctoSquidBehaviourScript : MonoBehaviour 
+{
+	GameObject playerObject;
+
+	// Use this for initialization
+	void Start () 
+	{
+		playerObject =  GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		UpdateMovement();
+	}
+	
+	void UpdateMovement()
+	{
+		Vector3 moveDirection = playerObject.transform.position - this.transform.position;
+		
+	}
+}
