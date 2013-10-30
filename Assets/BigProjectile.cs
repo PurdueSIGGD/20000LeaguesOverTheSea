447fe09f6e2e5baf3aada64ae660de2365976ffa
tragessere <tragessere@7d2ba457-d7c3-4905-95e4-8f938edf1c07>
2013-10-30 23:32:35 +0000

    Space Game: Projectile class for the big bullet fired in BigBulletShoot.

---
+++ Assets/BigProjectile.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class BigProjectile : MonoBehaviour {
+	int i = 8;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnCollisionEnter(Collision coll)
+    {
+        Collider other = coll.collider;
+		BasicCollision otherCollider= other.gameObject.GetComponent<BasicCollision>();
+		//if other object has a collision behavior, hit it in whatever way it wants.
+		if (otherCollider!=null)
+		{
+			//should take into account what type of projectile this is.
+			otherCollider.hit (this.gameObject);
+			
+		}
+        else
+        {
+            
+           // GameObject.DestroyObject(coll.gameObject);
+			
+		}
+		
+
+		i--;
+		if(String.Compare(coll.gameObject.ToString(), this.gameObject.ToString(), false)==0){
+			GameObject.DestroyObject(this.gameObject);
+		}
+		if(i == 6 || coll.gameObject.tag != "Bullet") {	//Evan's amazing work, you can get autographs on monday wednesday and saturday
+			GameObject.DestroyObject(this.gameObject);
+		}
+    } 
+}
