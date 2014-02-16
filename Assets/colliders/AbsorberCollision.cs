08860c62534da26acbf60e08750593d6d1893b7d
unknown <nrishel@K9-25.ad.cs.purdue.edu>
2014-02-15 20:52:28 -0500

    Removing unnecessar Update and Start functions from Collisions.
    Removed OnCollisionEnter from BasicCollision and adjusted overrides.

--- Assets/colliders/AbsorberCollision.cs
+++ Assets/colliders/AbsorberCollision.cs
@@ -2,17 +2,6 @@
 using System.Collections;
 
 public class AbsorberCollision : BasicCollision {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-	
 	public override void hit(GameObject collider){
 		if(collider.tag == "Bullet"){
 			this.GetComponent<Absorber>().gotHit();
--- Assets/colliders/PlanetCollision.cs
+++ Assets/colliders/PlanetCollision.cs
@@ -5,11 +5,6 @@ public class PlanetCollision : BasicCollision {
 	
 	public int health = 100;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
 	void Update () {
 		if (health <= 0) {
