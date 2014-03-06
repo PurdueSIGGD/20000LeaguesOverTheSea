a435a0d6c0c4775d0a5b6fcfaf88a46f22b4cb01
Simon Smith <smithsps@gmail.com>
2014-03-05 19:16:44 -0500

    Implemented the rest of orbit besides line drawing, have not tested.

--- Assets/Orbit2.cs
+++ Assets/Orbit2.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class Orbit : MonoBehaviour {
+public class Orbit2 : MonoBehaviour {
 
 	//Custom center of gravity, if set it overrides tagged planets
 	public GameObject customGravityAnchor; 
@@ -15,29 +15,59 @@ public class Orbit : MonoBehaviour {
 	// TODO: gravity contant is the mass of the planet
 
 	public bool drawLine = false;
-
-	//Init 
+	
 	void Start () {
-
-
 		//If customGravityAnchor is not null make it the only member of the gravityAnchors
 		if (customGravityAnchor != null) {
 			gravityAnchors = new GameObject[1];
 			gravityAnchors[0] = customGravityAnchor;
 		} else {
-			//All GameObjects with the tag Planet are added to the array
+			//Unity editor's tag system
 			gravityAnchors = GameObject.FindGameObjectsWithTag("Planet");
 		}
+
+		applyPerpForce(initialPerpForce);
+	}
+
+	void FixedUpdate(){
+
+		//Get the force vector from all gravitational bodies
+		Vector3 force = Vector3.zero;
+		foreach (GameObject go in gravityAnchors) {
+			force += gravitied(go);
+		}
+
+		// NOTE: ForceMode Velocity Change ignores this object's mass so gravity 
+		//			is applied the same across all objects
+		this.rigidbody.AddForce(force, ForceMode.VelocityChange);
 	}
 
 
-
 	//Apply a force perpendicular the gravitational anchors
-	void applyPerpForce(int force) {
-		//Calcuate the center of gravity based on distance and gravity const.
-		Vector3 centerOfGravity = new Vector();
-		foreach (GameObject gravObj in gravityAnchors) {
-			centerOfGravity
+	//	Used to start the object off orbiting around the planet and not falling straight into it.
+	void applyPerpForce(float force) {
+
+		//Find which gravity object has the largest influence on the craft
+		GameObject closestPlanet = gravityAnchors[0];
+		Vector3 max = gravitied(closestPlanet); //Max dist vector
+		foreach (GameObject go in gravityAnchors) {
+			if (max.magnitude < gravitied(go).magnitude) {
+				closestPlanet = go;
+				max = gravitied(go);
+			}
 		}
+
+		//Rotates the craft to planet vector 90 degrees, tis a neat vector trick
+		Vector3 perpDirection = new Vector3(-max.y, max.x, 0).normalized;
+
+		this.rigidbody.AddForce(perpDirection * force, ForceMode.VelocityChange);
+	}
+
+	//Calculate the Gravitational Force!
+	//Simplified to planet mass / dist ^ 2 * vectorNorm(direction of craft to planet)
+	Vector3 gravitied(GameObject go) {
+		Vector3 vector = go.rigidbody.position - this.rigidbody.position;
+		float magnitude = go.rigidbody.mass / vector.sqrMagnitude;
+		return magnitude * vector.normalized;
 	}
 }
