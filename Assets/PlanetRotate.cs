1fc79466ba3a3f302454aaf7ed08e9af4bcc86a7
TylerWolverton <twolvert@purdue.edu>
2014-03-29 19:39:44 -0400

    Added rotation to planet. Added in the materials for the first 5 stages

---
+++ Assets/PlanetRotate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetRotate : MonoBehaviour 
+{
+	public float speed = 1;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		//speed = speed + 20;
+		//planet.renderer.material.SetTextureOffset("CherryMaterial", new Vector2(speed,0));
+		transform.Rotate(0, speed, 0, Space.World);
+	}
+}
