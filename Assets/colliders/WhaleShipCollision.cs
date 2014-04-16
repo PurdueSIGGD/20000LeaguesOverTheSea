d2a93ad6be8bf1cc9cecf765597680facbbdfd36
TylerWolverton <twolvert@purdue.edu>
2014-04-15 20:00:09 -0400

    Added whale boss and started programming it

--- Assets/colliders/WhaleShipCollision.cs
+++ Assets/colliders/WhaleShipCollision.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 using System.Collections;
 
-public class WhaleShipCollision : BasicCollision {
-	public override void hit(GameObject collider){
-		if(collider.tag == "Bullet"){
+public class WhaleShipCollision : BasicCollision 
+{
+	public override void hit(GameObject collider)
+	{
+		if(collider.tag == "Bullet")
+		{
 			this.GetComponent<WhaleShip>().gotHit();
 		}
-		
+
 		if(collider.tag == "Player")
 		{
 			GameObject.Destroy(collider.gameObject);
