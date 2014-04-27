d4c08bbc86647c90674cd65f07fb305ae07cb623
Rickeon <Rickeon@gmail.com>
2014-04-26 22:38:20 -0400

    Basic Harpoon gun logic for whale boss

---
+++ Assets/BackgroundScroller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroller : MonoBehaviour {
+	public float xspeed, yspeed;
+	// Use this for initialization
+	void Start () {
+	
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		Rect b=this.GetComponent<GUITexture>().pixelInset;
+		b.x=b.x+xspeed;
+		b.y=b.y+yspeed;
+		this.GetComponent<GUITexture>().pixelInset=b;
+	}
+}
