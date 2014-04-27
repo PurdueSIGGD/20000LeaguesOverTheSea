02fce06dcf7333a2a7f0119fd086a03b6d76eb6c
Simon Smith <smithsps@gmail.com>
2014-04-26 21:17:56 -0400

    Added border on Respawn Timer; fixed bug where crashing into the planet while invincible

--- Assets/colliders/PlayerCollision.cs
+++ Assets/colliders/PlayerCollision.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PlayerCollision : BasicCollision{
 	public override void hit(GameObject collider)
 	{
-		if(!PlayerSpawner.isInvincible)
+		if(!PlayerSpawner.isInvincible || collider.tag == "Planet")
 		{
 			this.GetComponent<PlayerControl>().resetInput();
 			this.gameObject.SetActive(false);
