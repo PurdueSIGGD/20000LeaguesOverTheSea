a7c04d2df3927e179fd9e94d70d9fa1b18deb98f
TylerWolverton <twolvert@purdue.edu>
2014-10-30 22:18:47 -0400

    Took out colored backgrounds

--- Assets/RandomizeBackgroundTexture.cs
+++ Assets/RandomizeBackgroundTexture.cs
@@ -6,15 +6,13 @@ public class RandomizeBackgroundTexture : MonoBehaviour {
 	string[] backgrounds = { "Backgrounds/Background Concept 1",
 						     "Backgrounds/Background Concept 2",
 						     "Backgrounds/Background Concept 3",
-						     "Backgrounds/Background Concept 4",
-						     "Backgrounds/Background Concept 5",
-						     "Backgrounds/Background Concept 6" };
+						      };
 	
 	
 	// Use this for initialization
 	void Start () 
 	{
-		int backNum = Random.Range (0, 6);
+		int backNum = Random.Range (0, 3);
 		gameObject.guiTexture.texture = Resources.Load(backgrounds[backNum], typeof(Texture)) as Texture;
 	}
 }
